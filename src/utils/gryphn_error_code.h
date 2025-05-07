142a147500bed2ebd631844d49fb358e5ae4f02a
Greg Wells <gregory@Gregs-MacBook-Pro.local>
2025-05-06 20:50:13 -0400

    remove the comment of the old structure

--- src/utils/gryphn_error_code.h
+++ src/utils/gryphn_error_code.h
@@ -1,26 +1,3 @@
-// #include <utils/gryphn_bool.h>
-// #include <utils/gryphn_access_level.h>
-// #include <vector>
-// #include <utils/strings/gryphn_string.h>
-
-// enum gnReturnCodeLevel { GN_FAILED = 0, GN_SUCCESS = 1 };
-
-// inline gnString lastReturnCode = gnCreateString();
-
-// typedef struct gnReturnCode {
-// ACCESS_LEVEL:
-//     gnBool success;
-//     gnString returnCodeMessage = "";
-// public:
-//     bool operator==(gnReturnCodeLevel level) { return success; }
-//     gnReturnCode(gnReturnCodeLevel level, gnString message) : success(level), returnCodeMessage(message) { lastReturnCode = returnCodeMessage; }
-//     gnReturnCode(gnReturnCodeLevel level) : success(level) { lastReturnCode = returnCodeMessage; }
-//     gnReturnCode() { lastReturnCode = returnCodeMessage; }
-// } gnErrorCode;
-
-// gnString gnGetErrorString();
-// static gnReturnCode gnReturnError(gnString errorMessage) { return { GN_FAILED, errorMessage }; }
-
 #include "utils/strings/gryphn_string.h"
 
 typedef enum gnReturnCode {
