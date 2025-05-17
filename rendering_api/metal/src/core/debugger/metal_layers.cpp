41f464226056076bf8c1446e4fffe5591b038836
Greg Wells <gregory@Gregs-MacBook-Pro.local>
2025-05-17 13:22:12 -0400

    fix warnning about C style classes

--- rendering_api/metal/src/core/debugger/metal_layers.cpp
+++ rendering_api/metal/src/core/debugger/metal_layers.cpp
@@ -1,6 +1,7 @@
+#undef GN_UTILS_CPP
 #include <core/debugger/gryphn_layers.h>
 
 GN_EXPORT gnString gnGetPlatformLayerNameFn(const gnString& gnName) {
-    if (gnStringEquals(gnName, "GN_DEFAULT_DEBUG_LAYER")) return "METAL_DEBUG_LAYER";
-    return "GN_NO_LAYER";
+    if (gnStringEquals(gnName, "GN_DEFAULT_DEBUG_LAYER")) return gnCreateString("METAL_DEBUG_LAYER");
+    return gnCreateString("GN_NO_LAYER");
 }
