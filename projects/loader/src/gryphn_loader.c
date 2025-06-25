dee0822779d0823633d658673496c545c84d6a5f
Greg Wells <gregory@Gregs-MacBook-Pro.local>
2025-06-25 10:53:45 -0400

    get the metal loader to kinda work (does not load functions)

--- projects/loader/src/gryphn_loader.c
+++ projects/loader/src/gryphn_loader.c
@@ -1,5 +1,10 @@
 #include "gryphn_loader.h"
+#ifdef GN_API_VULKAN
 #include <apis/vulkan/loader/vulkan_loader.h>
+#endif
+#ifdef GN_API_METAL
+#include <apis/metal/loader/metal_loader.h>
+#endif
 
 gnInstanceFunctions loadInstanceFunctions(loaderInfo info) {
     switch (info.api) {
@@ -10,7 +15,10 @@ gnInstanceFunctions loadInstanceFunctions(loaderInfo info) {
     case GN_RENDERINGAPI_DIRECTX11: return (gnInstanceFunctions){ NULL };
     case GN_RENDERINGAPI_DIRECTX12: return (gnInstanceFunctions){ NULL };
     case GN_RENDERINGAPI_OPENGL: return (gnInstanceFunctions){ NULL };
-    case GN_RENDERINGAPI_METAL: return (gnInstanceFunctions){ NULL };
+#ifdef GN_API_METAL
+    case GN_RENDERINGAPI_METAL: return loadMetalInstanceFunctions();
+#endif
+    default: return (gnInstanceFunctions){NULL};
     }
 }
 
@@ -23,7 +31,11 @@ gnDeviceFunctions loadDeviceFunctions(loaderInfo info) {
     case GN_RENDERINGAPI_DIRECTX11: return (gnDeviceFunctions){ NULL };
     case GN_RENDERINGAPI_DIRECTX12: return (gnDeviceFunctions){ NULL };
     case GN_RENDERINGAPI_OPENGL: return (gnDeviceFunctions){ NULL };
-    case GN_RENDERINGAPI_METAL: return (gnDeviceFunctions){ NULL };
+#ifdef GN_API_METAL
+    case GN_RENDERINGAPI_METAL: return loadMetalDeviceFunctions();
+#endif
+
+    default: return (gnDeviceFunctions){NULL};
     }
 }
 
@@ -36,6 +48,10 @@ gnCommandFunctions loadCommandFunctions(loaderInfo info) {
     case GN_RENDERINGAPI_DIRECTX11: return (gnCommandFunctions){ NULL };
     case GN_RENDERINGAPI_DIRECTX12: return (gnCommandFunctions){ NULL };
     case GN_RENDERINGAPI_OPENGL: return (gnCommandFunctions){ NULL };
-    case GN_RENDERINGAPI_METAL: return (gnCommandFunctions){ NULL };
+#ifdef GN_API_METAL
+    case GN_RENDERINGAPI_METAL: return loadMetalCommandFunctions();
+#endif
+
+    default: return (gnCommandFunctions){NULL};
     }
 }
