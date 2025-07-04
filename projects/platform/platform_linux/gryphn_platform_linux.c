7313964c7afc8d290a993c382324e216b6d8c063
Gregory Wells <me@gregorywells.dev>
2025-07-03 21:36:06 -0400

    gnSupportsRenderingAPI on linux

--- projects/platform/platform_linux/gryphn_platform_linux.c
+++ projects/platform/platform_linux/gryphn_platform_linux.c
@@ -2,14 +2,20 @@
 #include "gryphn_platform_linux.h"
 #include "gryphn_rendering_api.h"
 
-gnRenderingAPI renderingAPIs[2] = {
+gnRenderingAPI renderingAPIs[3] = {
     GN_RENDERINGAPI_VULKAN,
+    GN_RENDERINGAPI_OPENGL,
     GN_RENDERINGAPI_SOFTWARE
 };
 
 gnRenderingAPI* gnGetSupportedRenderingAPIs(int* count) {
-    *count = 2;
+    *count = 3;
     return renderingAPIs;
+}
+
+gnBool gnSupportsRenderingAPI(gnRenderingAPI api) {
+    for (int i = 0; i < 3; i++) if (api == renderingAPIs[i]) return gnTrue;
+    return gnFalse;
 }
 
 #endif
