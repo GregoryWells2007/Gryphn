adf72d64363ce6017fd6dc0ff5b51c47e87b985a
Gregory Wells <me@gregorywells.dev>
2025-08-12 20:32:00 -0400

    add more stufff to utils

--- projects/apis/metal/src/shader_module/metal_shader_compiler.h
+++ projects/apis/metal/src/shader_module/metal_shader_compiler.h
@@ -1,13 +1,8 @@
 #pragma once
 #include "stdint.h"
 #include "utils/gryphn_bool.h"
+#include "utils/gryphn_cpp_function.h"
 #include <core/src/uniforms/gryphn_uniform_layout.h>
-
-#ifdef __cplusplus
-#define GN_CPP_FUNCTION extern "C"
-#else
-#define GN_CPP_FUNCTION
-#endif
 
 #define MAX_METAL_SETS 32
 #define MAX_METAL_BINDINGS 16
@@ -30,33 +25,3 @@ typedef struct mtlCompilerInfo {
 
 GN_CPP_FUNCTION mtlCompiler mtlCreateCompiler(mtlCompilerInfo* info);
 GN_CPP_FUNCTION const char* mtlCompilerShader(mtlCompiler compiler, gnUniformLayout* uniformLayout);
-
-// typedef struct mtlShaderOptions {
-//     gnBool useArgumentBuffers;
-//     mtlShaderModuleStage stage;
-//     const char* entryPoint;
-// } mtlShaderOptions;
-
-// typedef struct mtlBinding {
-//     uint32_t spvBinding;
-//     uint32_t metalID;
-// } mtlBinding;
-
-// typedef struct mtlSetMap {
-//     uint32_t setIndex, mtlSetIndex;
-//     mtlBinding bindings[MAX_METAL_BINDINGS];
-// } mtlSetMap;
-
-// typedef struct mtlShaderMap {
-//     mtlSetMap sets[MAX_METAL_SETS];
-// } mtlShaderMap;
-
-// typedef struct mtlShader {
-//     const char* code;
-//     mtlShaderMap map;
-// } mtlShader;
-
-// #ifdef __cplusplus
-// extern "C"
-// #endif
-// mtlShader mtlCompileShader(uint32_t* code, size_t wordCount, mtlShaderOptions* options);
